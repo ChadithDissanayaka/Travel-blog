8574883e9cf4d6452299eed1430f0c38f8876981
Chadith Dissanayaka <chadithdissanayaka@gmail.com>
2025-05-21 16:08:41 +0530

    added get authoer and profile pic, commnets using postid.

--- Server/services/commentService.js
+++ Server/services/commentService.js
@@ -1,13 +1,13 @@
-// services/commentService.js
 const commentDAO = require('../dao/commentDAO');
 const logger = require('../utils/logger');
+const authDAO = require('../dao/authDAO');
 
 class CommentService {
     async addComment(userId, postId, commentText) {
         try {
-            const result = await commentDAO.addComment(userId, postId, commentText);
+            const comment = await commentDAO.addComment(userId, postId, commentText);
             logger.info(`Comment added by user ${userId} on post ${postId}`);
-            return result;
+            return comment;
         } catch (error) {
             logger.error(`Error adding comment on post ${postId}: ${error.message}`);
             throw new Error(`Error adding comment: ${error.message}`);
@@ -17,6 +17,11 @@ class CommentService {
     async getCommentsForPost(postId) {
         try {
             const comments = await commentDAO.getCommentsForPost(postId);
+            for (let comment of comments) {
+                const user = await authDAO.findUserById(comment.user_id);
+                comment.author = user.username;
+                comment.profile_picture = user.profile_picture;
+            }
             logger.info(`Comments fetched for post ${postId}`);
             return comments;
         } catch (error) {
