63203470985a4601fa61cba9c387691231e3e101
Chadith Dissanayaka <chadithdissanayaka@gmail.com>
2025-05-21 16:18:16 +0530

    added  password reset route and send Set JWT and CSRF tokens in cookies.

--- Server/routes/apiKeyRoutes.js
+++ Server/routes/apiKeyRoutes.js
@@ -1,4 +1,3 @@
-// routes/apiKeyRoutes.js
 const express = require('express');
 const router = express.Router();
 const apiKeyService = require('../services/apiKeyService');
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,18 +1,17 @@
-// routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
 const authService = require('../services/authService');
-const logger = require('../utils/logger'); // Import the logger
+const logger = require('../utils/logger');
 
 // Register route
 router.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const result = await authService.register({ username, email, password });
-    logger.info(`New user registered: ${username}`); // Log user registration
+    logger.info(`New user registered: ${username}`);
     res.json(result);
   } catch (error) {
-    logger.error(`Registration failed for ${username}: ${error.message}`); // Log error
+    logger.error(`Registration failed for ${username}: ${error.message}`);
     res.status(500).json({ error: error.message });
   }
 });
@@ -23,18 +22,48 @@ router.post('/login', async (req, res) => {
   try {
     const result = await authService.login(username, password);
     if (!result) {
-      logger.warn(`Failed login attempt for user: ${username}`); // Log failed login
+      logger.warn(`Failed login attempt for user: ${username}`);
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
     // Set JWT and CSRF tokens in cookies
-    res.cookie('jwt', result.accessToken, { httpOnly: true, sameSite: 'strict', secure: true });
-    res.cookie('csrf-token', result.csrfToken, { httpOnly: false, sameSite: 'strict', secure: true });
+    res.cookie('jwt', result.accessToken, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24 // 1 day
+    });
 
-    logger.info(`User logged in: ${username}`); // Log successful login
-    res.json({ message: 'Login successful', user: result.user, csrfToken: result.csrfToken, apiKey: result.apiKey  });
+    res.cookie('csrf-token', result.csrfToken, {
+      httpOnly: false,
+      sameSite: 'strict',
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 // 1 Day
+    });
+
+
+    logger.info(`User logged in: ${username}`);
+    res.json({ message: 'Login successful', user: result.user, csrfToken: result.csrfToken, apiKey: result.apiKey });
   } catch (error) {
-    logger.error(`Login failed for ${username}: ${error.message}`); // Log error
+    logger.error(`Login failed for ${username}: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/reset-password', async (req, res) => {
+  const { email, newPassword } = req.body;
+  try {
+    const result = await authService.resetPassword(email, newPassword);
+
+    if (result) {
+      logger.info(`Password reset successful for user with email: ${email}`);
+      return res.json({ message: 'Password reset successfully' });
+    } else {
+      logger.warn(`Password reset failed for email: ${email}`);
+      return res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    logger.error(`Password reset failed for ${email}: ${error.message}`);
     res.status(500).json({ error: error.message });
   }
 });
@@ -43,7 +72,7 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   res.clearCookie('jwt');
   res.clearCookie('csrf-token');
-  logger.info('User logged out'); // Log logout
+  logger.info('User logged out');
   res.json({ message: 'Logged out successfully' });
 });
 
